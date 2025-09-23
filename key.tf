4e345722be5877e0f6b33ba2108c78171d985354
jawwad78 <ja66730@gmail.com>
2025-09-23 09:41:45 +0100

    Stop tracking key.tf

--- key.tf
+++
@@ -1,6 +0,0 @@
-# Using locally generated SSH key (created with ssh-keygen) 
-# Private key is stored in ~/.ssh/my-key
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"                      
-  public_key = file("~/.ssh/id_rsa.pub")     
-}
